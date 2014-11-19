aef84cb02ea6388701cace96dfe47c6c3d3de1a4
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2014-11-19 02:35:09 +0000

    Minimal todo list app, using $scope

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 (function() {
   // Define an angular module with a list of dependencies
-  angular.module('app', []);
+  angular
+    .module('app', [])
+    .controller('TodoListCtrl', TodoListCtrl);
+
+  function TodoListCtrl($scope) {
+    $scope.todos = [];
+    $scope.newTodo = null;
+
+    $scope.addTodo = function(todo) {
+      $scope.todos.push(todo);
+      $scope.newTodo = null;
+    };
+  }
 })();
