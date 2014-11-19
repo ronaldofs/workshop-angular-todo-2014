22eb67a3c4ae20d642cbd2a0eacc509499a9eb57
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2014-11-19 16:18:16 +0000

    Refactor new todo form as directive with controller and isolate scope

--- js/app.js
+++ js/app.js
@@ -5,23 +5,16 @@
     .controller('TodoListCtrl', TodoListCtrl)
     .factory('TodoList', TodoList)
     .constant('TodoFilters', _getTodoFilters())
-    .value('TodoModel', _getTodoModel());
+    .value('TodoModel', _getTodoModel())
+    .directive('gbTodoForm', TodoForm)
+    .controller('TodoFormCtrl', TodoFormCtrl);
 
-  function TodoListCtrl(TodoList, TodoFilters, TodoModel) {
+  function TodoListCtrl(TodoList, TodoFilters) {
     var ctrl = this;
 
     ctrl.todos = TodoList.items;
-    ctrl.newTodo = new TodoModel();
-    ctrl.addTodo = addTodo;
-
     ctrl.filters = TodoFilters;
     ctrl.selectedFilter = ctrl.filters[0];
-
-    function addTodo(todo) {
-      TodoList.addTodo(todo);
-
-      ctrl.newTodo = new TodoModel();
-    }
   }
 
   function TodoList() {
@@ -52,6 +45,33 @@
     }
 
     return Todo;
+  }
+
+  function TodoForm() {
+    return {
+      restrict: 'A',
+      scope: true,
+      template:
+        '<form class="gb-todo-form">' +
+          '<input type="text" ng-model="ctrl.newTodo.title">' +
+          '<button ng-click="ctrl.addTodo()">Add new todo</button>' +
+        '</form>',
+      controller: 'TodoFormCtrl',
+      controllerAs: 'ctrl'
+    };
+  }
+
+  function TodoFormCtrl(TodoModel, TodoList) {
+    var ctrl = this;
+
+    ctrl.newTodo = new TodoModel();
+    ctrl.addTodo = addTodo;
+
+    function addTodo() {
+      TodoList.addTodo(ctrl.newTodo);
+
+      ctrl.newTodo = new TodoModel();
+    }
   }
 
 })();
